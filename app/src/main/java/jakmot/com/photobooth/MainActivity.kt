caf77aaf2829c7e7dc1874eb9b154ccf39871e48
jakub.motulewicz <jakubmotulewicz@gmail.com>
2021-04-15 23:06:38 +0200

    add buttons for navigation

--- app/src/main/java/jakmot/com/photobooth/MainActivity.kt
+++ app/src/main/java/jakmot/com/photobooth/MainActivity.kt
@@ -2,10 +2,19 @@ package jakmot.com.photobooth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.takePhoto).setOnClickListener {
+            Toast.makeText(this, "Take a photo", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.seePhotos).setOnClickListener {
+            Toast.makeText(this, "See photos", Toast.LENGTH_SHORT).show()
+        }
     }
 }
