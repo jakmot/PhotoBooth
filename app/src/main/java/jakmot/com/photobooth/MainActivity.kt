8eed474bd129ee9160495bd72390f53adc40f7ea
jakub.motulewicz <jakubmotulewicz@gmail.com>
2021-05-01 22:11:22 +0200

    add navigation file

--- app/src/main/java/jakmot/com/photobooth/MainActivity.kt
+++ app/src/main/java/jakmot/com/photobooth/MainActivity.kt
@@ -2,19 +2,11 @@ package jakmot.com.photobooth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import jakmot.com.photobooth.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.host_activity)
-
-        val homeFragment = HomeFragment()
-
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.content, homeFragment)
-            .commit()
     }
 }
