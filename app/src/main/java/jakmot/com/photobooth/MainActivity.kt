ac4eff8abda34b018187089ba8a8c7786a5fa74b
jakub.motulewicz <jakubmotulewicz@gmail.com>
2021-05-01 23:44:13 +0200

    add up button to toolbar

--- app/src/main/java/jakmot/com/photobooth/MainActivity.kt
+++ app/src/main/java/jakmot/com/photobooth/MainActivity.kt
@@ -2,11 +2,23 @@ package jakmot.com.photobooth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.host_activity)
+        setContentView(R.layout.main_activity)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
